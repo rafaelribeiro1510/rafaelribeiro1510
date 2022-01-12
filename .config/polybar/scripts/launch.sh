1f987dfe2a3a2ed0a7a4e403bfcd2a2247d3e0fb
Rafael Ribeiro <45789887+up201806330@users.noreply.github.com>
2022-01-12 19:20:04 +0000

    Script for launching polybar in 2 screens

---
+++ .config/polybar/scripts/launch.sh
@@ -0,0 +1,39 @@
+#!/bin/env sh
+# Detect if secondary monitor is connected, if so, add a specific bar and move tray to it
+# Else, keep tray on main monitor
+# see https://github.com/polybar/polybar/issues/763
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+set -- $outputs
+tray_output=$1
+
+for m in $outputs; do
+	if [ $m == $1 ] 
+	then		
+		MONITOR1=$m polybar --reload main &	
+	elif [ $m == $2 ]
+	then
+	    tray_output=$m
+		MONITOR2=$m polybar --reload secondary &
+	else
+		MONITOR1=$m polybar --reload main &
+  fi
+done
+
+for m in $outputs; do
+export MONITOR1=$1
+export MONITOR2=$2
+export TRAY_POSITION=none
+if [[ $m == $tray_output ]]; then
+   TRAY_POSITION=right
+fi
+done
+
+sleep 1;
+
+polybar i3wmthemer_bar &
